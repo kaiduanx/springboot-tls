30039e9c421bd6a2a8a4b5f91970b2c48cac5324
Kaiduan Xie <kaiduanx@goodstartsoft.com>
2022-05-31 16:12:52 -0400

    Use BouncyCastle TLS implementation with Spring Boot.

--- example-1/src/main/java/com/example/demo/DemoApplication.java
+++ example-1/src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController // Change 1: Added this annotation
 public class DemoApplication {
 
+	static {
+		java.security.Security.insertProviderAt(new org.bouncycastle.jce.provider.BouncyCastleProvider(), 2);
+		java.security.Security.insertProviderAt(new org.bouncycastle.jsse.provider.BouncyCastleJsseProvider(), 3);
+	}
+
 	public static void main(String[] args) {
+		java.security.Provider[] providers = java.security.Security.getProviders();
+
+		for (java.security.Provider provider : providers) {
+			System.err.println("Installed security providers:" +
+			provider.getInfo() + "\n");
+		}
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
@@ -19,6 +30,6 @@ public class DemoApplication {
 	 */
 	@GetMapping
 	public String sayHello() {
-		return "Hello world!";
+		return "Hello world OpenQKDNetwork BouncyCastle Spring boot!!!!\n";
 	}
 }
