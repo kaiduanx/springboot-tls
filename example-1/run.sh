9a01b83fdf5bf33f81316c459f71866b42128505
Kaiduan Xie <kaiduanx@goodstartsoft.com>
2022-11-20 13:51:37 -0500

    Run p521_kyber1024.

--- example-1/run.sh
+++ example-1/run.sh
@@ -1,1 +1,2 @@
-java -Djava.util.logging.config.file=logging.properties  -Djdk.tls.namedGroups=x25519 -jar target/demo-0.0.1-SNAPSHOT.jar 
+java -Djava.util.logging.config.file=logging.properties  -Djdk.tls.namedGroups=p521_kyber1024 -jar target/demo-0.0.1-SNAPSHOT.jar
+#java -Djava.util.logging.config.file=logging.properties  -Djdk.tls.namedGroups=x25519 -jar target/demo-0.0.1-SNAPSHOT.jar
